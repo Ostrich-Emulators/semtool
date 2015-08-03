085f269f9b8b83f13d4f5cdfd92220f538eefe97
Wayne Warren <Mit$1655>
2015-08-03 08:34:17 -0700

    Added functions to the grid playsheet...
    .... that support the custom copy, paste, and select actions.

--- src/main/java/gov/va/semoss/ui/components/playsheets/GridPlaySheet.java
+++ src/main/java/gov/va/semoss/ui/components/playsheets/GridPlaySheet.java
@@ -22,13 +22,25 @@ package gov.va.semoss.ui.components.playsheets;
 import gov.va.semoss.rdf.engine.api.IEngine;
 import gov.va.semoss.ui.components.models.ValueTableModel;
 import gov.va.semoss.ui.components.renderers.LabeledPairTableCellRenderer;
-
 import gov.va.semoss.util.MultiSetMap;
+
+import java.awt.Cursor;
+import java.awt.HeadlessException;
+import java.awt.Toolkit;
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.UnsupportedFlavorException;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
-
 import java.util.Map;
+
 import javax.swing.JTable;
+import javax.swing.table.JTableHeader;
+import javax.swing.table.TableModel;
 
 import org.apache.log4j.Logger;
 import org.openrdf.model.Resource;
@@ -54,6 +66,9 @@ public class GridPlaySheet extends GridRAWPlaySheet {
 		super( new ValueTableModel( false ) );
 		JTable table = getTable();
 		table.setDefaultRenderer( URI.class, renderer );
+		// In order to enable the excel-style mouse interactions, we'll set
+		// the enhanced table interactions here
+		setEnhancedUserInteractions(table);
 	}
 
 	@Override
@@ -129,6 +144,149 @@ public class GridPlaySheet extends GridRAWPlaySheet {
 			}
 			return true;
 		}
+	}
+	
+	private void setEnhancedUserInteractions(JTable table) {
+		final JTableHeader header = table.getTableHeader();
+		//table.setCellEditor(new CustomCellEditor());
+		header.setReorderingAllowed(false);
+		table.addKeyListener(new TableKeyListener(table));
+		header.addMouseListener(new MouseAdapter() {
+			public void mouseClicked(MouseEvent e) {
+				int col = header.columnAtPoint(e.getPoint());
+				System.out.printf("click cursor = %d%n", header.getCursor()
+						.getType());
+				if (header.getCursor().getType() == Cursor.E_RESIZE_CURSOR)
+					e.consume();
+				else {
+					// System.out.printf("sorting column %d%n", col);
+					table.setColumnSelectionAllowed(false);
+					table.setRowSelectionAllowed(true);
+					table.clearSelection();
+					table.setColumnSelectionInterval(col, col);
+					// tableModel[selectedTab].sortArrayList(col);
+				}
+			}
+		});
+	}
+	
+	private void setCellEditor(){
+		
+	}
+	
+	
+	class AltInteractionTable extends JTable
+	{
+		AltInteractionTable(Object[][] data, String[] columnNames)
+	    {
+	        super(data, columnNames);
+	        // That's already the default        
+	        //	        setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
+	    }
 
+	    /**
+	     * Called by javax.swing.plaf.basic.BasicTableUI.Handler.adjustSelection(MouseEvent)
+	     * like: table.changeSelection(pressedRow, pressedCol, e.isControlDown(), e.isShiftDown());
+	     */
+	    @Override
+	    public void changeSelection(int rowIndex, int columnIndex, boolean toggle, boolean extend)
+	    {
+	        if (toggle && !isRowSelected(rowIndex)){
+	            return; // Don't do the selection
+	        }
+	        super.changeSelection(rowIndex, columnIndex, toggle, extend);
+	    }
+	}
+	
+	class TableKeyListener implements KeyListener {
+
+		public boolean CTRL_PRESSED = false;
+		
+		private final TableModel model;
+		
+		private final JTable table;
+		
+		public TableKeyListener(JTable table){
+			this.model = table.getModel();
+			this.table = table;
+		}
+		
+		@Override
+		public void keyTyped(KeyEvent e) {
+			// Do nothing, conditions are handled by keyPressed
+		}
+
+		@Override
+		public void keyPressed(KeyEvent e) {
+			int keycode = e.getKeyCode();
+			if (keycode == KeyEvent.CTRL_DOWN_MASK){
+				CTRL_PRESSED = true;
+			}
+			if (keycode == KeyEvent.VK_C){
+				if (CTRL_PRESSED){
+					initiateCopy();
+				}
+			}
+			if (keycode == KeyEvent.VK_V){
+				if (CTRL_PRESSED){
+					initiatePaste();
+				}
+			}
+		}
+
+		@Override
+		public void keyReleased(KeyEvent e) {
+			int keycode = e.getKeyCode();
+			if (keycode == KeyEvent.CTRL_DOWN_MASK){
+				CTRL_PRESSED = false;
+			}
+		}
+		
+		public void initiateCopy(){
+			
+			table.updateUI();
+		}
+		
+		public void initiatePaste(){
+			try {
+				String data = (String) Toolkit.getDefaultToolkit()
+				        .getSystemClipboard().getData(DataFlavor.stringFlavor);
+				int rowIndex = table.getSelectedRow();
+				if (rowIndex >= 0){
+					System.out.println(data);
+				}
+			} catch (HeadlessException e) {
+				log.error("Error - applicaiton is running headless.");
+				log.error(e);
+			} catch (UnsupportedFlavorException e) {
+				log.error("Error - Unsupported data flavor encountered during paste action.");
+				log.error(e);
+			} catch (IOException e) {
+				log.error("Error - IO Exception encountered during paste action.");
+				log.error(e);
+			} 
+		}
+		
+		public void initiateInsert(){
+			try {
+				String data = (String) Toolkit.getDefaultToolkit()
+				        .getSystemClipboard().getData(DataFlavor.stringFlavor);
+				int rowIndex = table.getSelectedRow();
+				if (rowIndex >= 0){
+					System.out.println(data);
+				}
+			} catch (HeadlessException e) {
+				log.error("Error - applicaiton is running headless.");
+				log.error(e);
+			} catch (UnsupportedFlavorException e) {
+				log.error("Error - Unsupported data flavor encountered during paste action.");
+				log.error(e);
+			} catch (IOException e) {
+				log.error("Error - IO Exception encountered during paste action.");
+				log.error(e);
+			} 
+			
+		}
+		
 	}
 }
