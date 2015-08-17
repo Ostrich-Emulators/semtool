960dcb53be625e6d32286b36dd40e3fb7e999e8d
quest5020 <JohnMarquiss@gmail.com>
2015-08-17 18:59:14 +0200

    Updates to db server

--- webapp/src/main/java/gov/va/semoss/web/controller/HomeController.java
+++ webapp/src/main/java/gov/va/semoss/web/controller/HomeController.java
@@ -5,9 +5,25 @@
 package gov.va.semoss.web.controller;
 
 import org.apache.log4j.Logger;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.google.gson.Gson;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import gov.va.semoss.web.io.DbInfo;
 
 /**
  *
@@ -16,10 +32,51 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class HomeController extends SemossControllerBase {
 
-  private static final Logger log = Logger.getLogger( HomeController.class );
+	private static final Logger log = Logger.getLogger( HomeController.class );
+	private static ArrayList<Map<String, Object>> knowledgeBases;
 
-  @RequestMapping( value = "/", method = RequestMethod.GET )
-  public String getWelcome() {
-    return "index.vm";
-  }
+	@RequestMapping( value = "/", method = RequestMethod.GET )
+	public String getWelcome() {
+		return "index.vm";
+	}
+
+	@RequestMapping( value = "/testDriver", method = RequestMethod.GET )
+	public String getTestDriver() {
+		return "testDriver.vm";
+	}
+	
+	@RequestMapping( value = "/semoss/allDatabaseIDs", method = RequestMethod.GET )
+	public @ResponseBody String getAllDatabaseIDs() {
+		ArrayList<Map<String, Object>> knowledgeBases = DbInfo.getTestDatabases();
+		
+		ArrayList<String> theDatabaseIDs = new ArrayList<String>();
+		for (Map<String, Object> knowledgeBase: knowledgeBases) {
+			theDatabaseIDs.add( knowledgeBase.get("name")+"" );
+		}
+		
+		String json = new Gson().toJson( theDatabaseIDs );
+		return json;
+	}
+	
+	@RequestMapping( value = "/semoss/allDatabases", method = RequestMethod.GET )
+	public @ResponseBody String getAllDatabases() {
+		ArrayList<Map<String, Object>> knowledgeBases = DbInfo.getTestDatabases();
+		
+		String json = new Gson().toJson( knowledgeBases );
+		return json;
+	}
+	
+	@RequestMapping( value = "/semoss/oneDatabase/{id}", method = RequestMethod.GET )
+	public @ResponseBody String getOneDatabaseWithID(@PathVariable("id") String id) {
+		ArrayList<Map<String, Object>> knowledgeBases = DbInfo.getTestDatabases();
+
+		for (Map<String, Object> knowledgeBase: knowledgeBases) {
+			if ( knowledgeBase.get("name").equals(id) ) {
+				return new Gson().toJson( knowledgeBase );
+			}
+		}
+		
+		return new Gson().toJson( DbInfo.getEmptyDatabase().getAsMap() );
+	}
+
 }
