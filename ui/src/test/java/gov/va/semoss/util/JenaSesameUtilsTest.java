771b38b58b80bf6ff5350b8bae39ba900bb3e365
Wayne Warren <Mit$1655>
2015-09-25 13:44:00 -0700

    Fixed the instantiation issues
    ... surrounding the subjects, objects, predicates

--- ui/src/test/java/gov/va/semoss/util/JenaSesameUtilsTest.java
+++ ui/src/test/java/gov/va/semoss/util/JenaSesameUtilsTest.java
@@ -6,50 +6,68 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.hp.hpl.jena.rdf.model.ModelFactory;
 import com.hp.hpl.jena.rdf.model.Property;
 import com.hp.hpl.jena.rdf.model.Resource;
+import com.hp.hpl.jena.rdf.model.ResourceFactory;
 import com.hp.hpl.jena.rdf.model.Statement;
+import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.impl.PropertyImpl;
-import com.hp.hpl.jena.rdf.model.impl.ResourceImpl;
 import com.hp.hpl.jena.rdf.model.impl.StatementImpl;
 import com.hp.hpl.jena.rdf.model.RDFNode;
+
 import org.openrdf.model.impl.URIImpl;
+
 import org.openrdf.model.URI;
 
 public class JenaSesameUtilsTest {
 
-	private static final Resource jenaResource = new ResourceImpl("Mantech");
+	private static RDFNode jenaResource;
 	
-	private static final  Property jenaPredicate = new PropertyImpl("is_a");
+	private static  Property jenaPredicate;
 	
-	private static final RDFNode jenaSubject = new ResourceImpl("Company");
+	private static Resource jenaSubject;
 	
-	private static final org.openrdf.model.Resource sesameResource = new org.openrdf.model.impl.BNodeImpl("Mantech");
+	private static org.openrdf.model.Resource sesameResource;
 	
 	private static URI sesamePredicate;
 	
 	private static final org.openrdf.model.Resource sesameSubject = new org.openrdf.model.impl.BNodeImpl("Mantech");
 	
+	private static final Model testingModel = ModelFactory.createDefaultModel();
+	
 	@Before
 	public void setUp() throws Exception {
-		sesamePredicate = new URIImpl("rdf:type");
-		PropertyImpl prop = new PropertyImpl("name");
-		jenaResource.addProperty(prop, "Mantech");
+		jenaResource = testingModel.createResource("class:Company");
+		jenaSubject = testingModel.createResource("thing:Mantech");
+		jenaPredicate = new PropertyImpl("rdf:is_a");
 	}
 	
 	@Test
 	public void testAsSesameResource() {
-		org.openrdf.model.Resource sResource = JenaSesameUtils.asSesameResource(jenaResource);
-		String value = sResource.stringValue();
-		assertEquals(value, "Mantech");
+		org.openrdf.model.Resource sResource = JenaSesameUtils.asSesameResource(jenaSubject);
+		String s = sResource.stringValue();
+		assertEquals(s, "thing:Mantech");
 	}
 	
 	@Test
 	public void testAsSesameStatement(){
-		org.openrdf.model.Statement sStatement = JenaSesameUtils.asSesameStatement(this.getJenaStatement());
-		assertEquals(sStatement.getObject().stringValue(), "Company");
-		assertEquals(sStatement.getSubject().stringValue(), "Mantech");
+		Statement sStatement = getJenaStatement();
+		org.openrdf.model.Statement tStatement = JenaSesameUtils.asSesameStatement(sStatement);
+		String object = tStatement.getObject().stringValue();
+		String subject = tStatement.getSubject().stringValue();
+		String predicate = tStatement.getPredicate().stringValue();
+		assertEquals(object, "class:Company");
+		assertEquals(subject, "thing:Mantech");
+		assertEquals(predicate, "rdf:is_a");
 	}
+
+	@Test
+	public void testAsSesameURI(){
+		org.openrdf.model.URI sURI = JenaSesameUtils.asSesameURI(jenaPredicate);
+		assertEquals(sURI.stringValue(), "rdf:is_a");
+	}
+	
 	
 	@Test
 	public void testAsJenaStatement(){
@@ -58,13 +76,13 @@ public class JenaSesameUtilsTest {
 	}
 	
 	private Statement getJenaStatement(){
-		Statement stmt = new StatementImpl(jenaResource, jenaPredicate, jenaSubject);
+		Statement stmt = new StatementImpl(jenaSubject, jenaPredicate, jenaResource);
 		return stmt;
 	}
 	
 	private org.openrdf.model.Statement getSesameStatement(){
 		org.openrdf.model.Statement stmt = 
-				new org.openrdf.model.impl.StatementImpl(sesameResource, sesamePredicate ,sesameSubject);
+				new org.openrdf.model.impl.StatementImpl(sesameSubject, sesamePredicate , sesameResource);
 		return stmt;
 	}
 
