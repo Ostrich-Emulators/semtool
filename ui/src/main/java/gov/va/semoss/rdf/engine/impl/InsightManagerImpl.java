36655173aa1276bfa696a2b2f959fd530edc429f
ry99 <ryan@ostrich-emulators.com>
2015-09-28 16:31:48 -0400

    use metamodel graph style

--- ui/src/main/java/gov/va/semoss/rdf/engine/impl/InsightManagerImpl.java
+++ ui/src/main/java/gov/va/semoss/rdf/engine/impl/InsightManagerImpl.java
@@ -312,7 +312,7 @@ public class InsightManagerImpl implements InsightManager {
 				InsightOutputType.GRAPH_METAMODEL );
 
 		Insight explore = new Insight( "Explore an instance of a selected node type",
-				"DESCRIBE ?instance", InsightOutputType.GRAPH );
+				"DESCRIBE ?instance", InsightOutputType.GRAPH_METAMODEL );
 		explore.setId( urib.uniqueUri() );
 
 		Parameter concept = new Parameter( "Concept",
