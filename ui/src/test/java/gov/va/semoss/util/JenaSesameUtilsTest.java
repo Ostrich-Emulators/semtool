3acc8183727669b8e70c81dab14a6e683ff84df9
Wayne Warren <Mit$1655>
2015-09-28 23:51:24 -0700

    Added more JenaSesameUtil tests

--- ui/src/test/java/gov/va/semoss/util/JenaSesameUtilsTest.java
+++ ui/src/test/java/gov/va/semoss/util/JenaSesameUtilsTest.java
@@ -17,8 +17,10 @@ import com.hp.hpl.jena.rdf.model.impl.StatementImpl;
 import com.hp.hpl.jena.rdf.model.RDFNode;
 
 import org.openrdf.model.impl.URIImpl;
-
+import org.openrdf.model.impl.ValueFactoryImpl;
+import org.openrdf.model.Graph;
 import org.openrdf.model.URI;
+import org.openrdf.model.ValueFactory;
 
 public class JenaSesameUtilsTest {
 
@@ -32,15 +34,19 @@ public class JenaSesameUtilsTest {
 	
 	private static URI sesamePredicate;
 	
-	private static final org.openrdf.model.Resource sesameSubject = new org.openrdf.model.impl.BNodeImpl("Mantech");
+	private static org.openrdf.model.Resource sesameSubject = new org.openrdf.model.impl.BNodeImpl("Mantech");
 	
 	private static final Model testingModel = ModelFactory.createDefaultModel();
 	
+	private static final ValueFactory FACTORY = new ValueFactoryImpl();
 	@Before
 	public void setUp() throws Exception {
 		jenaResource = testingModel.createResource("class:Company");
 		jenaSubject = testingModel.createResource("thing:Mantech");
 		jenaPredicate = new PropertyImpl("rdf:is_a");
+		sesameResource = FACTORY.createBNode("class:Company");
+		sesameSubject = FACTORY.createBNode("thing:Mantech");
+		sesamePredicate = FACTORY.createURI("rdf:is_a");
 	}
 	
 	@Test
@@ -68,11 +74,36 @@ public class JenaSesameUtilsTest {
 		assertEquals(sURI.stringValue(), "rdf:is_a");
 	}
 	
+	@Test
+	public void testAsJenaNode(){
+		RDFNode node = JenaSesameUtils.asJenaNode(sesameSubject);
+		assertEquals(node.toString(), sesameSubject.stringValue());		
+	}
+	
+	@Test
+	public void testAsJenaResource(){
+		Resource resource = JenaSesameUtils.asJenaResource(sesameResource);
+		String s1 = resource.toString();
+		String s2 = sesameResource.stringValue();
+		assertEquals(s1, s2);
+	}
+	
 	
 	@Test
 	public void testAsJenaStatement(){
-		
-		
+		org.openrdf.model.Statement sesameStatement = getSesameStatement();
+		Statement jenaStatement = JenaSesameUtils.asJenaStatement(sesameStatement);
+		String obj1 = sesameStatement.getObject().stringValue();
+		String obj2 = jenaStatement.getObject().toString();
+		assertEquals(obj1, obj2);
+	}
+	
+	@Test
+	public void testAsSesameGraph(){
+		Graph sesameGraph = JenaSesameUtils.asSesameGraph(testingModel);
+		long size = sesameGraph.size();
+		long otherSize = testingModel.size();
+		assertEquals(otherSize, size);
 	}
 	
 	private Statement getJenaStatement(){
@@ -85,5 +116,7 @@ public class JenaSesameUtilsTest {
 				new org.openrdf.model.impl.StatementImpl(sesameSubject, sesamePredicate , sesameResource);
 		return stmt;
 	}
+	
+	
 
 }
