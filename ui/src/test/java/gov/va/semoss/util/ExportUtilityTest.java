2595b6c7a9f698324e24f6072ac72c82b14b06a9
ry99 <ryan@ostrich-emulators.com>
2015-09-29 08:23:20 -0400

    avoid HeadlessException on server

--- ui/src/test/java/gov/va/semoss/util/ExportUtilityTest.java
+++ ui/src/test/java/gov/va/semoss/util/ExportUtilityTest.java
@@ -40,12 +40,9 @@ public class ExportUtilityTest {
 
 	@Test
 	public void testFileName() {
-		TestPlaySheet sheet = new TestPlaySheet();
-		File file = ExportUtility.getSuggestedFilename( sheet, ".pdf" );
-		if ( !file.getName().contains( sheet.getTitle() ) ) {
-			fail( "File name must contain the title of the component from which "
-					+ "it is created." );
-		}
+		File file = ExportUtility.getSuggestedFilename( "A Test File", ".pdf" );
+		assertTrue( "File name must contain the title of the component from which it is created",
+				file.getName().contains( "A Test File" ) );
 	}
 
 	@Test
@@ -53,89 +50,6 @@ public class ExportUtilityTest {
 		testPDFFile = File.createTempFile( "export-test-", ".pdf" );
 		ExportUtility.exportAsPdf( image, testPDFFile );
 		assertTrue( "File did not save properly.", testPDFFile.exists() );
-		assertNotEquals( "File is 0-lenght.", 0, testPDFFile.length() );
+		assertNotEquals( "File is 0-length.", 0, testPDFFile.length() );
 	}
-
-	private class TestPlaySheet extends JFrame implements IPlaySheet {
-
-		private String title = "ATestingTitle";
-
-		@Override
-		public IEngine getEngine() {
-			return null;
-		}
-
-		@Override
-		public void setTitle( String title ) {
-			this.title = title;
-		}
-
-		@Override
-		public String getTitle() {
-			return this.title;
-		}
-
-		@Override
-		public boolean hasChanges() {
-			return false;
-		}
-
-		@Override
-		public Map<String, Action> getActions() {
-			return null;
-		}
-
-		@Override
-		public List<String> getHeaders() {
-			return null;
-		}
-
-		@Override
-		public void activated() {
-		}
-
-		@Override
-		public void create( List<Value[]> data, List<String> headers,
-				IEngine engine ) {
-		}
-
-		@Override
-		public void create( Model m, IEngine engine ) {
-		}
-
-		@Override
-		public void overlay( List<Value[]> data, List<String> headers,
-				IEngine eng ) {
-		}
-
-		@Override
-		public void overlay( Model m, IEngine engine ) {
-		}
-
-		@Override
-		public void incrementFont( float incr ) {
-		}
-
-		@Override
-		public boolean canAcceptDataWithHeaders( List<String> newheaders ) {
-			return false;
-		}
-
-		@Override
-		public boolean canAcceptModelData() {
-			return false;
-		}
-
-		@Override
-		public List<Object[]> getTabularData() {
-			return null;
-		}
-
-		@Override
-		public boolean prefersTabs() {
-			return false;
-		}
-
-	}
-
 }
