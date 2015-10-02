c33f9a9d914d56580dfd0b2dbaa1fa04755a1106
ry99 <ryan@ostrich-emulators.com>
2015-10-02 13:04:56 -0400

    more unit tests

--- common/src/main/java/gov/va/semoss/rdf/engine/util/RepositoryCopier.java
+++ common/src/main/java/gov/va/semoss/rdf/engine/util/RepositoryCopier.java
@@ -5,8 +5,6 @@
  */
 package gov.va.semoss.rdf.engine.util;
 
-import com.bigdata.rdf.model.BigdataValueFactory;
-import com.bigdata.rdf.model.BigdataValueFactoryImpl;
 import org.apache.log4j.Logger;
 import org.openrdf.model.Statement;
 import org.openrdf.repository.RepositoryConnection;
@@ -19,88 +17,86 @@ import org.openrdf.rio.RDFHandlerException;
  */
 public class RepositoryCopier implements RDFHandler {
 
-  private static final Logger log = Logger.getLogger( RepositoryCopier.class );
-  private final RepositoryConnection conn;
-  private int commitlimit = 10000;
-  private int adds = 0;
-  private int totalAdds = 0;
+	private static final Logger log = Logger.getLogger( RepositoryCopier.class );
+	private final RepositoryConnection conn;
+	private int commitlimit = 10000;
+	private int adds = 0;
+	private int totalAdds = 0;
 
-  /**
-   * Creates an instance
-   *
-   * @param rc
-   */
-  public RepositoryCopier( RepositoryConnection rc ) {
-    conn = rc;
-  }
+	/**
+	 * Creates an instance
+	 *
+	 * @param rc
+	 */
+	public RepositoryCopier( RepositoryConnection rc ) {
+		conn = rc;
+	}
 
-  public void setCommitLimit( int lim ) {
-    commitlimit = lim;
-  }
+	public void setCommitLimit( int lim ) {
+		commitlimit = lim;
+	}
 
-  @Override
-  public void startRDF() throws RDFHandlerException {
-    adds = 0;
-    totalAdds = 0;
-    try {
-      conn.begin();
-    }
-    catch ( Exception e ) {
-      throw new RDFHandlerException( e );
-    }
-  }
+	@Override
+	public void startRDF() throws RDFHandlerException {
+		adds = 0;
+		totalAdds = 0;
+		try {
+			conn.begin();
+		}
+		catch ( Exception e ) {
+			throw new RDFHandlerException( e );
+		}
+	}
 
-  @Override
-  public void endRDF() throws RDFHandlerException {
-    try {
-      log.debug( "committing " + totalAdds + " statements..." );
-      conn.commit();
-    }
-    catch ( Exception e ) {
-      throw new RDFHandlerException( e );
-    }
-  }
+	@Override
+	public void endRDF() throws RDFHandlerException {
+		try {
+			log.debug( "committing " + totalAdds + " statements..." );
+			conn.commit();
+		}
+		catch ( Exception e ) {
+			throw new RDFHandlerException( e );
+		}
+	}
 
-  @Override
-  public void handleNamespace( String string, String string1 ) throws RDFHandlerException {
-    try {
-      conn.setNamespace( string, string1 );
-    }
-    catch ( Exception e ) {
-      throw new RDFHandlerException( e );
-    }
-  }
+	@Override
+	public void handleNamespace( String string, String string1 ) throws RDFHandlerException {
+		try {
+			conn.setNamespace( string, string1 );
+		}
+		catch ( Exception e ) {
+			throw new RDFHandlerException( e );
+		}
+	}
 
-  @Override
-  public void handleStatement( Statement stmt ) throws RDFHandlerException {
-    try {
-      // RPB: I have no idea why we have to rebox these statements
-      BigdataValueFactory fac = BigdataValueFactoryImpl.getInstance( "kb" );
+	@Override
+	public void handleStatement( Statement stmt ) throws RDFHandlerException {
+		try {
+			// RPB: need to rebox these statements because BigData blows up if
+			// you try to copy one statement to another repository
+			conn.add( EngineLoader.cleanStatement( stmt, conn.getValueFactory() ) );
+			adds++;
+			totalAdds++;
+			if ( adds >= commitlimit ) {
+				log.debug( "committed " + totalAdds + " statements..." );
+				conn.commit();
+				conn.begin();
+				adds = 0;
+			}
+		}
+		catch ( Exception e ) {
+			try {
+				conn.rollback();
+			}
+			catch ( Exception ex ) {
+				throw new RDFHandlerException( ex );
+			}
+			throw new RDFHandlerException( e );
+		}
+	}
 
-      conn.add( fac.createStatement( stmt.getSubject(), stmt.getPredicate(),
-          stmt.getObject() ) );
-      adds++;
-      totalAdds++;
-      if ( adds >= commitlimit ) {
-        log.debug( "committed " + totalAdds + " statements..." );
-        conn.commit();
-        conn.begin();
-        adds = 0;
-      }
-    }
-    catch ( Exception e ) {
-      try {
-        conn.rollback();
-      }
-      catch ( Exception ex ) {
-        throw new RDFHandlerException( ex );
-      }
-      throw new RDFHandlerException( e );
-    }
-  }
-
-  @Override
-  public void handleComment( String string ) throws RDFHandlerException {
-    log.warn( "comments are not yet supported" );
-  }
+	@Override
+	public void handleComment( String string ) throws RDFHandlerException {
+		log.warn( "comments are not yet supported" );
+	}
 }
