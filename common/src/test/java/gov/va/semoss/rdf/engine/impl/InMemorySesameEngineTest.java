9805f1093ab69bb3feb405351c49d07663dc1e66
ry99 <ryan@ostrich-emulators.com>
2015-10-06 14:25:53 -0400

    unit tests

--- common/src/test/java/gov/va/semoss/rdf/engine/impl/InMemorySesameEngineTest.java
+++ common/src/test/java/gov/va/semoss/rdf/engine/impl/InMemorySesameEngineTest.java
@@ -5,20 +5,34 @@
  */
 package gov.va.semoss.rdf.engine.impl;
 
+import gov.va.semoss.rdf.engine.api.MetadataConstants;
+import static gov.va.semoss.rdf.query.util.QueryExecutorAdapter.getDate;
 import gov.va.semoss.rdf.query.util.UpdateExecutorAdapter;
+import gov.va.semoss.util.Constants;
+import gov.va.semoss.util.UriBuilder;
+import info.aduna.iteration.Iterations;
 import java.io.File;
+import java.util.Date;
+import java.util.List;
 import org.apache.log4j.Logger;
 import org.junit.After;
 import org.junit.AfterClass;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.openrdf.model.Literal;
+import org.openrdf.model.Statement;
+import org.openrdf.model.URI;
 import org.openrdf.model.impl.LiteralImpl;
 import org.openrdf.model.impl.StatementImpl;
 import org.openrdf.model.vocabulary.RDFS;
+import org.openrdf.repository.Repository;
 import org.openrdf.repository.RepositoryConnection;
+import org.openrdf.repository.sail.SailRepository;
 import org.openrdf.rio.RDFFormat;
+import org.openrdf.sail.memory.MemoryStore;
 
 /**
  *
@@ -53,6 +67,10 @@ public class InMemorySesameEngineTest {
 		rc.add( new StatementImpl( RDFS.DOMAIN, RDFS.LABEL,
 				new LiteralImpl( "label" ) ) );
 
+		rc.remove( eng.getBaseUri(), MetadataConstants.DCT_MODIFIED, null );
+		rc.add( eng.getBaseUri(), MetadataConstants.DCT_MODIFIED,
+				rc.getValueFactory().createLiteral( new Date() ) );
+
 		rc.commit();
 	}
 
@@ -63,8 +81,50 @@ public class InMemorySesameEngineTest {
 
 	@Test
 	public void testUpdate() throws Exception {
-		assertEquals( 40, eng.getRawConnection().size() );
+		assertEquals( 41, eng.getRawConnection().size() );
 		eng.update( new UpdateExecutorAdapter( "DELETE DATA { rdfs:domain rdfs:label \"label\" } " ) );
-		assertEquals( 39, eng.getRawConnection().size() );
+		assertEquals( 40, eng.getRawConnection().size() );
+	}
+
+	@Test
+	public void testUpdateDate() throws Exception {
+		Date date = new Date();
+		assertEquals( 41, eng.getRawConnection().size() );
+		eng.update( new UpdateExecutorAdapter( "DELETE DATA { rdfs:domain rdfs:label \"label\" } " ) );
+		assertEquals( 40, eng.getRawConnection().size() );
+
+		List<Statement> stmts = Iterations.asList( eng.getRawConnection().
+				getStatements( eng.getBaseUri(), MetadataConstants.DCT_MODIFIED,
+						null, false ) );
+		Literal val = Literal.class.cast( stmts.get( 0 ).getObject() );
+		Date upd = getDate( val.calendarValue() );
+		assertTrue( upd.after( date ) );
+	}
+
+	@Test
+	public void testUpdateDate2() throws Exception {
+		Repository repo = new SailRepository( new MemoryStore() );
+		repo.initialize();
+		RepositoryConnection rc = repo.getConnection();
+		UriBuilder urib = UriBuilder.getBuilder( Constants.ANYNODE + "/" );
+		URI base = urib.uniqueUri();
+		Date now = new Date();
+		rc.add( new StatementImpl( base, MetadataConstants.DCT_MODIFIED,
+				rc.getValueFactory().createLiteral( now ) ) );
+
+		AbstractSesameEngine.updateLastModifiedDate( rc, null );
+
+		List<Statement> stmts = Iterations.asList( eng.getRawConnection().
+				getStatements( eng.getBaseUri(), MetadataConstants.DCT_MODIFIED,
+						null, false ) );
+		Literal val = Literal.class.cast( stmts.get( 0 ).getObject() );
+		Date upd = getDate( val.calendarValue() );
+
+		rc.close();
+		repo.shutDown();
+
+		// the /100 is to remove the ms, which aren't always the same because
+		// they're not stored in the RDF
+		assertEquals( now.getTime()/100, upd.getTime()/100 );
 	}
 }
--- common/src/test/java/gov/va/semoss/rdf/engine/impl/InsightManagerImplTest.java
+++ common/src/test/java/gov/va/semoss/rdf/engine/impl/InsightManagerImplTest.java
@@ -5,9 +5,15 @@
  */
 package gov.va.semoss.rdf.engine.impl;
 
+import gov.va.semoss.om.Perspective;
+import gov.va.semoss.rdf.engine.api.InsightManager;
+import gov.va.semoss.user.LocalUserImpl;
+import gov.va.semoss.util.Constants;
+import gov.va.semoss.util.UriBuilder;
 import gov.va.semoss.util.Utility;
 import java.io.File;
 import java.io.IOException;
+import java.util.Collection;
 import java.util.Properties;
 import org.apache.commons.io.FileUtils;
 import org.junit.After;
@@ -16,8 +22,8 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.openrdf.model.Statement;
 import org.openrdf.repository.Repository;
-import org.openrdf.repository.RepositoryConnection;
 import org.openrdf.repository.sail.SailRepository;
 import org.openrdf.sail.memory.MemoryStore;
 
@@ -63,14 +69,31 @@ public class InsightManagerImplTest {
 		FileUtils.copyFile( DATAFILE, datafile );
 
 		Repository repo = new SailRepository( new MemoryStore( datadir ) );
-		repo.initialize();
-		RepositoryConnection rc = repo.getConnection();
-		InsightManagerImpl imi = new InsightManagerImpl();
-		imi.loadFromRepository( rc );
-		rc.close();
+		InsightManager imi = InsightManagerImpl.createFromRepository( repo );
 		repo.shutDown();
 
 		FileUtils.deleteQuietly( datadir );
 		assertEquals( 1, imi.getPerspectives().size() );
+	}
+
+	@Test
+	public void testCreateStatements() throws Exception {
+		InsightManagerImpl imi = new InsightManagerImpl();
+		Properties props = Utility.loadProp( LEGACY_QUESTIONS );
+		imi.loadLegacyData( props );
+
+		Collection<Statement> stmts
+				= InsightManagerImpl.getStatements( imi, new LocalUserImpl() );
+		assertEquals( 1502, stmts.size() );
+	}
+
+	@Test
+	public void testSystemP() {
+		InsightManagerImpl imi = new InsightManagerImpl();
+		InMemorySesameEngine eng = new InMemorySesameEngine();
+		eng.setBuilders( UriBuilder.getBuilder( Constants.ANYNODE + "/data/" ),
+				UriBuilder.getBuilder( Constants.ANYNODE + "/schema/" ) );
+		Perspective p = imi.getSystemPerspective( eng );
+		assertEquals( 3, p.getInsights().size() );
 	}
 }
