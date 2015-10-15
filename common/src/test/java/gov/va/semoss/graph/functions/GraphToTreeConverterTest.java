d8ffad26651e71eeed267a55b77a5932a089eec9
ry99 <ryan@ostrich-emulators.com>
2015-10-15 15:48:15 -0400

    test printing

--- common/src/test/java/gov/va/semoss/graph/functions/GraphToTreeConverterTest.java
+++ common/src/test/java/gov/va/semoss/graph/functions/GraphToTreeConverterTest.java
@@ -11,8 +11,13 @@ import edu.uci.ics.jung.graph.Forest;
 import edu.uci.ics.jung.graph.Tree;
 import edu.uci.ics.jung.graph.util.EdgeType;
 import gov.va.semoss.util.UriBuilder;
+import java.io.StringWriter;
 import java.util.Arrays;
 import java.util.Collection;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.log4j.SimpleLayout;
+import org.apache.log4j.WriterAppender;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -87,6 +92,27 @@ public class GraphToTreeConverterTest {
 		Tree<URI, URI> tree = result.getTrees().iterator().next();
 		assertEquals( YURI, tree.getRoot() );
 		assertEquals( YUGO, tree.getChildren( YURI ).iterator().next() );
+	}
+
+	@Test
+	public void testPrint() {
+
+		DirectedGraph<URI, URI> graph = new DirectedSparseGraph<>();
+		graph.addVertex( YURI );
+		graph.addVertex( YUGO );
+		graph.addEdge( YPY, YURI, YUGO, EdgeType.DIRECTED );
+
+		Collection<URI> roots = Arrays.asList( YURI );
+		GraphToTreeConverter.Search search = GraphToTreeConverter.Search.DFS;
+		Forest<URI, URI> result = GraphToTreeConverter.convert( graph, roots, search );
+
+		Logger log = Logger.getLogger( GraphToTreeConverter.class );
+		StringWriter stringy = new StringWriter();
+		WriterAppender app = new WriterAppender( new SimpleLayout(), stringy );
+		log.setLevel( Level.DEBUG );
+		log.addAppender( app );
+		GraphToTreeConverter.printForest( result );
+		assertEquals( "DEBUG - http://semoss.va.gov/database/T44889381-85ce-43e3-893d-6267fd480660/Yuri\nDEBUG -   http://semoss.va.gov/database/T44889381-85ce-43e3-893d-6267fd480660/Yugo\n", stringy.toString() );
 	}
 
 }
